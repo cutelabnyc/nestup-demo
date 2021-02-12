9734aa282e5e57dedcf391d119cc4b1cf83a93bb
Sam Tarakajian <sam@cycling74.com>
2021-02-12 08:54:38 -0500

    Something approximating progress

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { NestupDrum } from "./components/NestupDrum";
-import "./style/nestup-demo.css";
+import React from "react";
 import "./style/sidebar.css";
-import { SharableContext } from "./contexts/sharable";
-import { ShareButton } from "./components/ShareButton";
-import { ExportButton } from "./components/ExportButton";
-import Sidebar from './components/Sidebar';
-import { PatternPicker } from "./components/PatternPicker";
-import { patterns } from "./data/patterns";
 import { AudioManagerContext } from "./contexts/audio";
+import { LeftMenu } from "./components/LeftMenu";
+import { NestupArea } from "./components/NestupArea";
+import "./style/nestup-demo.css";
 
 // kick - C2
 // snare - C#2
@@ -16,61 +11,12 @@ import { AudioManagerContext } from "./contexts/audio";
 // cowbell - D#2 
 
 export const App = ({ audioManager, initialState }) => {
-
-    const [sampler, setSampler] = useState(null);
-    const [appPatterns, setPatterns] = useState(initialState);
-
-    const onPick = (pattern) => {
-        setPatterns({
-            sequences: pattern.map(p => { return { text: p }})
-        });
-    };
-
-    useEffect(() => {
-        const createSampler = async () => {
-
-            const nextSampler = new audioManager.tone.Sampler({
-                urls: {
-                    C1: "https://freesound.org/data/previews/248/248142_3667999-lq.mp3",
-                    ["D1"]: "https://freesound.org/data/previews/387/387186_7255534-lq.mp3",
-                    ["F#1"]: "https://freesound.org/data/previews/397/397042_4357738-lq.mp3",
-                    ["D2"]: "https://freesound.org/data/previews/99/99766_29308-lq.mp3"
-                }
-            }).toDestination();
-
-            setSampler(nextSampler);
-        }
-        createSampler();
-    }, []);
-
     return (
-        <div className="App" id="outer-container">
-            <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
-            <div id="page-wrap">
-                <div className="vertical-align">
-                    <div className="nestup-demo-root">
-                        <div className="code-container">
-                            <NestupDrum note={"C1"} sampler={sampler} index={0} initialState={appPatterns} />
-                            <NestupDrum note={"D1"} sampler={sampler} index={1} initialState={appPatterns} />
-                            <NestupDrum note={"F#1"} sampler={sampler} index={2} initialState={appPatterns} />
-                            <NestupDrum note={"D2"} sampler={sampler} index={3} initialState={appPatterns} />
-                        </div>
-                    </div>
-                    <SharableContext.Consumer>
-                        { ({ state }) => 
-                            <div className="footer">
-                                <ShareButton state={state} />
-                                <ExportButton state={state} />
-                            </div> }
-                    </SharableContext.Consumer>
-                </div>
-
-                <AudioManagerContext.Consumer>
-                    {value => (
-                        <PatternPicker patterns={patterns} onPick={onPick} audioManager={ value } />
-                    )}
-                </AudioManagerContext.Consumer>
-            </div>
+        <div className="App">
+            <LeftMenu />
+            <AudioManagerContext.Consumer>
+                {value => <NestupArea audioManager={value} /> }
+            </AudioManagerContext.Consumer>
         </div>
-    );
+    )  
 };
--- src/index.js
+++ src/index.js
@@ -5,20 +5,9 @@ import { AudioManagerContext } from "./contexts/audio";
 import { SharableProvider } from "./contexts/sharable";
 import url from "url";
 
-const loc = new URL(window.location.href);
-const encodedState = loc.searchParams.get("state");
-let state;
-if (encodedState) {
-	try {
-		state = JSON.parse(atob(encodedState));
-	} catch (e) {}
-}
-
 ReactDOM.render(
 	<SharableProvider>
-		<AudioManagerContext.Consumer>
-			{value => <App audioManager={value} initialState={state} />}
-		</AudioManagerContext.Consumer>
+		<App />
 	</SharableProvider>,
 	document.getElementById("root")
 );
